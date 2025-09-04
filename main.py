a97284514ac4cbf6163e389b4dcb01f763d65902
Danxer <joelcorona27.dc@gmail.com>
2025-09-03 18:33:19 -0600

    Main file added
    Begun of the main file of the project.

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Software Engineering Window")
+root.geometry("200x50")
+
+label_software = tk.Label(root, text="Software Engineering")
+label_software.pack(pady=10)
+
+root.mainloop()
